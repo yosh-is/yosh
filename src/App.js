4cf1695a75468736d3b496130e47f662a14fd7e0
yosh <sakulemo@example.com>
2022-08-18 11:31:09 +0900

    change

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
+import { useState } from "react";
 import "./App.css";
 
+import Header from "./components/Header";
 import Main from "./components/Main";
 
 function App() {
+  const [darkMode, setDarkMode] = useState(true);
+
+  function toggleDarkMode() {
+    setDarkMode((prevMode) => !prevMode);
+  }
+
   return (
-    <div className="App">
-      <Main />
+    <div
+      className="App"
+      data-color-mode={darkMode ? "dark" : "light"}
+    >
+      <Header
+        darkMode={darkMode}
+        toggleDarkMode={toggleDarkMode}
+      />
+      <Main darkMode={darkMode} />
     </div>
   );
 }
